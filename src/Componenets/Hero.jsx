cb701d9b8d808b82af5d7d9e7836e8d03b915f3f
Teja <tejayerriboyina@gmail.com>
2025-12-14 14:41:53 +0530

    hero section updated

--- src/Componenets/Hero.jsx
+++ src/Componenets/Hero.jsx
@@ -5,14 +5,19 @@ import { BsDot } from "react-icons/bs";
 const Hero = () => {
 
     return(
-        <div className="min-h-screen">
+        <div className="min-h-screen flex flex-col">
             <div className="flex items-center justify-start text-orange-500">
                 <p className="flex items-center justify-center text-gray-600 underline"><BsDot color="green" />online</p>
-                <h2 className="px-7 p-2 border border-orange-500 w-fit rounded-3xl flex items-center justify-center gap-2 backdrop-blur-sm bg-orange-500/20 ml-auto mr-auto"> <BiChip width={50}/> Initializing Sequence</h2>
+                <h2 className="px-7 p-2 border border-orange-500 w-fit rounded-3xl flex items-center justify-center gap-2 backdrop-blur-sm bg-orange-500/20 ml-auto mr-[40%]"> <BiChip width={50}/> Initializing Sequence</h2>
             </div>
 
-            <div>
-
+            <div className="flex justify-center items-center text-center pt-20 text-9xl text-white font-extrabold">
+                <h1 className="flex flex-col gap-4">
+                    SPARK <br />
+                    <span className="bg-linear-to-r from-orange-600 via-orange-200 to-orange-600 bg-clip-text text-transparent">
+                        2026
+                    </span>
+                </h1>
             </div>
         </div>
     )
